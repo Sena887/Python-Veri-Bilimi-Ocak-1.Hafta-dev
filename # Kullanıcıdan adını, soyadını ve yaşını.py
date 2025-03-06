6e44ebcb4385b47f6e0cd923d9f533f9462d14d9
Sena Yıldız <yldzsena04@gmail.com>
2025-03-06 16:57:49 +0300

    dosya yüklendi

---
+++ # Kullanıcıdan adını, soyadını ve yaşını.py
@@ -0,0 +1,9 @@
+# Kullanıcıdan adını, soyadını ve yaşını al
+ad = input("Adınızı girin: ")
+soyad = input("Soyadınızı girin: ")
+yas = input("Yaşınızı girin: ")
+
+# Girilen bilgileri ekrana yazdır
+print(f"Adınız: {ad}")
+print(f"Soyadınız: {soyad}")
+print(f"Yaşınız: {yas}")
